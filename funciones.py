6fdc89ea7a40e101ad68c3578aaee9a3158cf051
Fminaurol <Fminaurol@users.noreply.github.com>
2021-12-02 20:01:00 -0600

    Se realizan modificaciones a las funciones de máximo y mínimo y se agrega una para determinar componentes necesarios

--- funciones.py
+++ funciones.py
@@ -14,10 +14,8 @@ import math
 
 def approx_eigval_max(matriz_covarianza,max_iters):
     z_k_minus_1=[]
-    getcontext().prec = 7
     eigen_val_ant=1
     eigen_val=0
-    eigen_vec=(np.zeros((1,5)))
     k=0
     #Se crea una lista que contenga el número de columnas = al número de renglones de la matriz
     #de covarianzas -1 y al final se agrega un uno para seguir el algoritmo de la potencia
@@ -39,8 +37,9 @@ def approx_eigval_max(matriz_covarianza,max_iters):
         k=k+1 
     return eigen_val,eigen_vec,matriz_covarianza
 
+
 #Método de Deflation
-def approx_eigval_min(matriz_covarianza,max_iters,eigen_val_1,eigen_vec_1):
+def approx_eigval_min_1(matriz_covarianza,max_iters,eigen_val_1,eigen_vec_1):
     z_k_minus_1=[]
     eigen_val_ant=1
     eigen_val=0
@@ -54,7 +53,8 @@ def approx_eigval_min(matriz_covarianza,max_iters,eigen_val_1,eigen_vec_1):
             z_k_minus_1.append(0)
     #Para el método de deflaition se elimina el primer eigenvalor de la matriz y la resultante
     # es una matriz que contine los eigenvalores {0,l2,l3...}
-    x1=np.array([[eigen_vec_1[0]],[eigen_vec_1[1]],[eigen_vec_1[2]],[eigen_vec_1[3]],[eigen_vec_1[4]]])
+    x1=eigen_vec_1
+    x1=x1.reshape(len(matriz_covarianza),1)
     u1=eigen_val_1*x1
     matriz_covarianza=matriz_covarianza-x1@u1.T
     #Se genera un nuevo ciclo para calcular el segundo eigenvalor a partir del método potencia
@@ -97,3 +97,14 @@ def graficas_normalizadas(datos):
     plt.xlabel("Observación")
     plt.ylabel("Cotización en bolsa (USD)")
     plt.show()
+
+def componente(sigma,eigen_val,acumulado):
+    j=0
+    Acumulada=eigen_val.sum()
+    acumulado=0
+    componente_necesario=0
+    while (j in range(len(sigma)) and acumulado<=.80):
+        acumulado=acumulado+eig_val[j]/Acumulada
+        componente_necesario=j+1
+        j+=1
+    return acumulado, componente_necesario
